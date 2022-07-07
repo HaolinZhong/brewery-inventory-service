dae9e9501adf21f42acf50cdea5a19c504bcc30d
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-06 20:52:01 -0400

    init project; create data transfer object and database entity

---
+++ src/main/java/hz/spring/breweryinventoryservice/BreweryInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package hz.spring.breweryinventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreweryInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hz/spring/breweryinventoryservice/BreweryInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hz.spring.breweryinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
