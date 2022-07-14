42f61111992f92dbf10a6b0beacca3a8ec996405
HaolinZhong <hz2771@cumc.columbia.edu>
2022-07-14 17:32:18 -0400

    handle allocation request

---
+++ src/main/java/hz/spring/breweryinventoryservice/service/AllocationListener.java
@@ -0,0 +1,40 @@
+package hz.spring.breweryinventoryservice.service;
+
+import hz.spring.breweryinventoryservice.config.JmsConfig;
+import hz.spring.common.event.AllocateBeerOrderRequest;
+import hz.spring.common.event.AllocateBeerOrderResult;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class AllocationListener {
+
+    private final AllocationService allocationService;
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.ALLOCATE_ORDER_QUEUE)
+    public void listen(AllocateBeerOrderRequest request) {
+        AllocateBeerOrderResult.AllocateBeerOrderResultBuilder builder = AllocateBeerOrderResult.builder();
+        builder.beerOrderDTO(request.getBeerOrderDTO());
+
+        try {
+            Boolean allocationResult = allocationService.allocateOrder(request.getBeerOrderDTO());
+            if (allocationResult) {
+                builder.pendingInventory(false);
+            } else {
+                builder.pendingInventory(true);
+            }
+        } catch (Exception e) {
+            log.error("Allocation failed for Order Id: " + request.getBeerOrderDTO().getId());
+            builder.allocationError(true);
+        }
+
+        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_RESPONSE_QUEUE, builder.build());
+    }
+
+}
